b75e8420e523048119fa7b932103c6ebb3737aa2
Daniel McBryde <mcbryded@gmail.com>
2016-04-05 16:42:32 -0500

    populate movie list with like buttons

---
+++ app/components/like-counter.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  score: 0,
+  up(score) {
+    this.set(`score`, this.score + 1);
+  },
+  down(score) {
+    this.set(`score`, this.score - 1);
+  },
+
+});
